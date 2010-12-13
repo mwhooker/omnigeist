8232c1612355c61c24e1f3c6f9aa90f51993890e
Matthew Hooker <mhooker@digg.com>
2010-12-13 02:47:31 -0500

    memcache adapter for httplib cache

--- app/lib/shorturl.py
+++ app/lib/shorturl.py
@@ -1,11 +1,29 @@
 import httplib2
+from google.appengine.api import memcache
 
 USER_AGENT = "Python/shorturl.py"
 # TODO: find file-like cache in GAE for this
-h = httplib2.Http()
-
 
 class ResolveException(Exception): pass
+
+class MemcacheAdapter(httplib2.FileCache):
+    def __init__(self, namespace=None, **kwargs):
+        self.client = memcache.Client()
+        if namespace:
+            self.namespace = namespace
+        else:
+           self.namespace = self.__class__.__name__
+
+    def delete(self, key):
+        self.client.delete(key, namespace=self.namespace)
+
+    def get(self, key):
+        return self.client.get(key, namespace=self.namespace)
+
+    def set(self, key, value):
+        self.client.set(key, value, namespace=self.namespace)
+
+h = httplib2.Http(MemcacheAdapter('shorturl'))
 
 def resolve(url):
 
