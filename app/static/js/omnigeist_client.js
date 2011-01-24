80aeade9d6d4b75b26ce2e3ef01b083b309c8ada
Matthew Hooker <mhooker@digg.com>
2011-01-23 21:19:40 -0500

    bookmarklet working minimally

--- app/static/js/omnigeist_client.js
+++ app/static/js/omnigeist_client.js
@@ -1,51 +1,55 @@
+/*
+ * TODO:
+ *  namespace this file. i.e. put it in a function
+ */
 omni_height = 150;
 omni_width = 700;
 _jq_script=document.createElement('script');
 _jq_script.type='text/javascript';
 _jq_script.src='https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js';
 document.documentElement.appendChild(_jq_script);
-var domLoaded = function (callback) {
-    /* Internet Explorer */
-    /*@cc_on
-    @if (@_win32 || @_win64)
-        document.write('<script id="ieScriptLoad" defer src="//:"><\/script>');
-        document.getElementById('ieScriptLoad').onreadystatechange = function() {
-            if (this.readyState == 'complete') {
-                callback();
-            }
-        };
-    @end @*/
-    /* Mozilla, Chrome, Opera */
-    if (document.addEventListener) {
-        document.addEventListener('DOMContentLoaded', callback, false);
-    }
-    /* Safari, iCab, Konqueror */
-    if (/KHTML|WebKit|iCab/i.test(navigator.userAgent)) {
-        var DOMLoadTimer = setInterval(function () {
-            if (/loaded|complete/i.test(document.readyState)) {
-                callback();
-                clearInterval(DOMLoadTimer);
-            }
-        }, 10);
-    }
-    /* Other web browsers */
-    window.onload = callback;
-};
 
-domLoaded(function() {
-        $('<div id="omnigeist"></div>').appendTo('body');
-        $('<div id="omnigeist-spacer"></div>').appendTo('body');
-        $('#omnigeist').css('top', $(window).height() - omni_height);
-        $('#omnigeist').css('left', ($(window).width() - omni_width)/2);
-        //$('#omnigeist').addClass('omnigeist');
-        //$('#omnigeist').append('<p></p>');
-        $.ajax({
-            url: "http://localhost:8084/top.js",
-            dataType: 'jsonp',
-            data: ({idx: '1', provider: 'digg', url: window.location.href}),
-            context: $('#omnigeist'),
-            success: function(data){
-                $(this).text(data['body']);
-                //alert(data['body']);
-            }});
+jqueryLoaded = function(callback) {
+    tryReady = function(time_elapsed) {
+        // Continually polls to see if jQuery is loaded.
+        if (typeof $ == "undefined") { // if jQuery isn't loaded yet...
+            if (time_elapsed <= 5000) {
+                setTimeout("tryReady(" + (time_elapsed + 200) + ")", 200);
+            } else {
+                alert("Timed out while loading jQuery.")
+            }
+        } else {
+            // jQuery loaded successfully
+            callback();
+        }
+    }
+    return tryReady(0);
+}
+
+comment_tpl = '<div class="omnigeist"><p><%=body%> &mdash; <%=author%></p></div>';
+
+update_comments = function(idx, provider) {
+    $.ajax({
+        url: "http://localhost:8084/top.js",
+        dataType: 'jsonp',
+        data: ({idx: idx, provider: provider, url: window.location.href}),
+        success: function(data){
+            $('.omnigeist').replaceWith(tmpl(comment_tpl, data));
+    }});
+}
+
+
+main = function() {
+    $('<div id="omnigeist"><div class="omnigeist"></div></div>').appendTo('body');
+    update_comments(1, 'reddit');
+    //$('#omnigeist').addClass('omnigeist');
+    //$('#omnigeist').append('<p></p>');
+}
+
+jqueryLoaded(function() {
+    $.getScript('http://localhost:8084/static/js/template.js', function() {
+        main();
     });
+});
+
+
